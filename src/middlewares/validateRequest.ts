4954f6e90c3d287ec5850da7d2a9c08351801d5c
Muhiuddin <muhiuddin.bd.2005@gmail.com>
2026-05-18 11:40:00 +0600

    feat: add upload middleware

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -4,11 +4,18 @@ const validateRequest =
     (schema: any) =>
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            await schema.parseAsync({
+            const parsed = await schema.parseAsync({
                 body: req.body,
                 query: req.query,
                 cookies: req.cookies,
             });
+            if (parsed.body) req.body = parsed.body;
+            if (parsed.query) {
+                for (const key in parsed.query) {
+                    req.query[key] = parsed.query[key];
+                }
+            }
+            if (parsed.cookies) req.cookies = parsed.cookies;
             return next();
         } catch (error) {
             next(error);
