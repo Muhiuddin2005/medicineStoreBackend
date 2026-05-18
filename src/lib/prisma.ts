d6dc6209026333bd64ed4fded5d1f238f2bb1e2f
Muhiuddin <muhiuddin.bd.2005@gmail.com>
2026-05-18 10:35:00 +0600

    chore: update prisma schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client'
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client/index.js";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+export { prisma };
