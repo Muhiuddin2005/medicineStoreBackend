79d4d087a3483db538b6ac4cf3b885d7dd584520
Muhiuddin <muhiuddin.bd.2005@gmail.com>
2026-06-21 11:22:33 +0600

    docs/fix: update server console.log to print localhost URL

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ async function main() {
         console.log("Connected to the database successfully.");
 
         app.listen(PORT as number, "0.0.0.0", () => {
-            console.log(`Server is running on http://0.0.0.0:${PORT}`);
+            console.log(`Server is running on http://localhost:${PORT}`);
         });
     } catch (error) {
         console.error("An error occurred:", error);
